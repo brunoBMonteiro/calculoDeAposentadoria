9e92ec94a97496e5232b564e78f35270914efa1c
Bruno Bilheri <52968377+brunoBMonteiro@users.noreply.github.com>
2020-09-07 17:11:35 -0300

    fazendo calculo de aposentadoria

---
+++ aposentadoria.js
@@ -0,0 +1,20 @@
+const nome = "Silvana";
+const sexo = "F";
+const idade = 48;
+const contribuicao = 23;
+
+const calculoAposentadoria = idade + contribuicao;
+
+const homenPodeAposentar = sexo == 'M' && contribuicao >= 35 && calculoAposentadoria >= 95;
+const mulherPodeAposentar = sexo == 'F' && contribuicao >= 35 && calculoAposentadoria >= 85;
+
+
+if (homenPodeAposentar) {
+    if(mulherPodeAposentar){
+        console.log('podem se aposentar')
+    } else {
+        console.log('homem pode se aposentar e a mulher não')
+    }
+}   else {
+    console.log('homem não pode se aposentar')
+}
